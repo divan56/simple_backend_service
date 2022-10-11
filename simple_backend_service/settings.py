49340c4e9ad611f9e5fa577f3851f10e169ef081
divan56 <dorohowmax@gmail.com>
2022-10-11 09:47:42 +0300

    added-cors

--- simple_backend_service/settings.py
+++ simple_backend_service/settings.py
@@ -106,16 +106,10 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
-]
-CORS_ALLOW_METHODS = [
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
+CORS_ALLOW_ALL_ORIGINS: True
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 # Internationalization
